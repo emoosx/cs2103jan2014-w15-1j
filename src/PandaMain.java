02f159c5ef0305ffb5014317d22412935646cc8b
Matthew <Matthew@localhost>
2014-02-24 14:34:12 +0800

    added showToUser and readUserInput methods

--- src/PandaMain.java
+++ src/PandaMain.java
@@ -1,7 +1,28 @@
-package storage;
+/* Task Panda v0.1
+ * Created 24/2/2014
+ * Contributors: Clement, Htet Kuang, Matthew
+ */
 
-// Main class
+import java.util.*;
 
 public class PandaMain {
-
+	
+	private static final String MESSAGE_WELCOME = "Welcome to Task Panda v0.0!";
+	
+	public static void main(String[] args) {
+		showToUser(MESSAGE_WELCOME);
+		while(true) {
+			String feedback = readUserCommand();
+			showToUser(feedback);
+		}
+	}
+	
+	private static void showToUser(String outputString) {
+		System.out.println(outputString);
+	}
+	
+	private static String readUserCommand() {
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLine();
+	}
 }
