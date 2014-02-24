158151b3c4c9b1988823de800e7f12612542ff64
Matthew <Matthew@localhost>
2014-02-24 15:14:03 +0800

    1st edition of parameter parser utilizes the JCommander Framework parse user inputs according to specific tags

---
+++ src/ParamParser.java
@@ -0,0 +1,14 @@
+/*
+ * ParamParser utilizes the JCommander Framework to parse user inputs according to specific tags
+ * 
+ */
+
+import com.beust.jcommander.*;
+
+public class ParamParser {
+	
+	// Stub
+	@Parameter (names = "-priority", description = "priority of tasks")
+	private String priority = null;
+	
+}
