4e9894378f557f82085c16ff529becca468fdb02
Matthew <Matthew@localhost>
2014-02-25 13:32:09 +0800

    First edition of task undo

---
+++ src/TaskUndo.java
@@ -0,0 +1,14 @@
+/*
+ * This class performs undo operations
+ * It will store deleted task objects into a stack 
+ * Stack will be popped when undo is executed
+ * V0.1 - Supports undo for 1 operation
+ */
+
+public class TaskUndo {
+	
+	public void run() {
+		
+	}
+	
+}
