ebd512475f2d3a8540d155ad6c2e42de8ce40cca
Matthew <Matthew@localhost>
2014-02-25 15:41:59 +0800

    added main parameter

--- src/ParamParser.java
+++ src/ParamParser.java
@@ -3,23 +3,27 @@
  * 
  */
 
+import java.util.ArrayList;
+
 import com.beust.jcommander.*;
 
 public class ParamParser {
+	
+	@Parameter(description = "Words that are not needed")
+	private ArrayList<String> files = new ArrayList<String>();
 	
 	// Stub
 	@Parameter (names = "-priority", description = "priority of tasks")
 	private String priority = null;
 	
 	/**************************** Add Parameters ****************************/
-	
-	@Parameter (names = "from", description = "getting start time")
+	@Parameter (names = "-from", description = "getting start time")
 	private String startTime = null;
 	
-	@Parameter (names = "to", description = "getting end time")
+	@Parameter (names = "-to", description = "getting end time")
 	private String endTime = null;
 	
-	@Parameter (names = { "on", "by" }, description = "getting deadline")
+	@Parameter (names = { "-on", "-by" }, description = "getting deadline")
 	private String endDate = null;
 
 	public String getStartTime() {
