3d9cb9a3fb92c029ca50c8826d0f2b934ac638d8
Clement <Clement.Chong73@gmail.com>
2014-02-24 16:44:06 -0800

    Added Task Object Class

--- src/Task.java
+++ src/Task.java
@@ -1,4 +1,40 @@
 
 public class Task {
 
+	private String taskName;
+	private String taskDescription;
+	//private Date time;
+	//private String priority;
+	
+	public Task(String tName, String tDesc){
+		this.taskName = tName;
+		this.taskDescription = tDesc;
+	}
+	
+	public void setTaskName(String tName){
+		this.taskName = tName;
+	}
+	
+	public String getTaskName(){
+		return this.taskName;
+	}
+	
+	public void setTaskDescription(String tDesc){
+		this.taskDescription = tDesc;
+	}
+	
+	public String getTaskDescription(){
+		return this.taskDescription;
+	}
+	public String toString() {
+		
+		        return new StringBuffer(" Task Name : ").append(this.taskName)
+		
+		                .append(" Task Description : ").append(this.taskDescription).
+		
+		               toString();
+		
+		    }
+
+	
 }
