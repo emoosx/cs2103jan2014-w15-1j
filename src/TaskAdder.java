bf77fc214e63e9cdb83f7e9b6ec42ccb862dc947
Matthew <Matthew@localhost>
2014-02-27 17:15:43 +0800

    added more details of different tasks added final strings for keywords created class attributes

--- src/TaskAdder.java
+++ src/TaskAdder.java
@@ -2,6 +2,18 @@
  * executeAdd class will perform its run method as a drive method
  * It will determine what kind of tasks to add and call on storage to store the data thereafter
  * Passes a task object to storage.java
+ * V0.1 - Supports specific keywords and date only
+ * 
+ * Floating tasks:
+ * add repair watch
+ * add wash car
+ * add go to gardens by the bay
+ * 
+ * Timed tasks:
+ * add meeting on 27-2-2014 from 2pm to 4pm
+ * 
+ * Deadline tasks:
+ * add submit minutes on 28-2-2014 by 8pm
  */
 
 public class TaskAdder {
@@ -9,6 +21,16 @@ public class TaskAdder {
 	private final String TASK_TYPE_FLOATING = "float"; 
 	private final String TASK_TYPE_TIMED = "timed";
 	private final String TASK_TYPE_DEADLINE = "deadline";
+	
+	private final String KEYWORD_BY = "by";
+	private final String KEYWORD_TO = "to";
+	private final String KEYWORD_FROM = "from";
+	private final String KEYWARD_ON = "on";
+	
+	private String taskDescription;
+	private int startTime;
+	private int endTime;
+	//private Date date;
 	
 	/*
 	 * Public method that will be called by Parser.java
@@ -25,35 +47,10 @@ public class TaskAdder {
 	 * Deadline tasks: tasks that has an end time
 	 */
 	private void determineTask(String inputString) {
-		String taskType = parseTask(inputString);
-		switch(taskType) {
-			case(TASK_TYPE_FLOATING):
-				addFloatingTask(inputString);
-				break;
-			case(TASK_TYPE_TIMED):
-				addTimedTask(inputString);
-				break;
-			case(TASK_TYPE_DEADLINE):
-				addDeadlineTask(inputString);
-				break;
-			default:
-				break;
-		}
+		checkFloatingTask(inputString);
 	}
 	
-	private String parseTask(String inputString) {
-		return inputString;
-	}
-	
-	private void addFloatingTask(String inputString) {
-		
-	}
-	
-	private void addTimedTask(String inputString) {
-		
-	}
-	
-	private void addDeadlineTask(String inputString) {
+	private void checkFloatingTask(String inputString) {
 		
 	}
 }
