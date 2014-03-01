667f4a525bcfa9a792111f6f75bbf620c97bf1fa
Matthew <Matthew@localhost>
2014-03-02 02:20:33 +0800

    Added more comments Edited readUserInput method to correctly close scanner class Changed welcome message to display v0.1 instead of v0.0

--- src/PandaMain.java
+++ src/PandaMain.java
@@ -1,13 +1,16 @@
 /* Task Panda v0.1
- * Created 24/2/2014
- * Contributors: Clement, Htet Kuang, Matthew
+ * Created on 24/2/2014
+ * Contributors: 
+ * Clement Chong - A0097784H
+ * Kuang Htet - A0105860L
+ * Tan Zheng Jie (Matthew) - A0101810A
  */
 
 import java.util.*;
 
 public class PandaMain {
 	
-	private static final String MESSAGE_WELCOME = "Welcome to Task Panda v0.0!";
+	private static final String MESSAGE_WELCOME = "Welcome to Task Panda v0.1!";
 	
 	public static void main(String[] args) {
 		showToUser(MESSAGE_WELCOME);
@@ -22,12 +25,16 @@ public class PandaMain {
 		}
 	}
 	
+	// This method print out the given argument
 	private static void showToUser(String outputString) {
 		System.out.println(outputString);
 	}
 	
+	// This method will read in 1 line of user input
 	private static String readUserInput() {
 		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
+		String userInput = sc.nextLine();
+		sc.close();
+		return userInput;
 	}
 }
