d1873cff601b9fe6ef01b3d9f9b929e1173c3973
Kaung Htet Zaw <emoosx@gmail.com>
2014-03-01 16:09:11 +0800

    Initial testing of json storage

---
+++ src/storage/Blah.java
@@ -0,0 +1,11 @@
+package storage;
+
+import core.Task;
+
+public class Blah {
+	public static void main(String[] args) {
+		StorageHelper storage = new StorageHelper();
+		Task newTask = new Task("test Task", "woowhooo");
+		storage.addNewTaskTest(newTask);
+	}
+}
