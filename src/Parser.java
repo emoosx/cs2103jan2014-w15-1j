6ec0dc20f5504bb86d4b9a823f753861389b4b2f
Matthew <Matthew@localhost>
2014-03-02 03:01:03 +0800

    Major refactoring: -shifted removeFirstWord method calls into executeCommand method to prevent indexOutOfBound exceptions on single word commands -removed MESSAGE_ERROR and shifted it into PandaMain.java -added MESSAGE_HELP and a help command -added displayHelp method -editted comments for clarity -added isInvalidString method to handle empty user inputs

--- src/Parser.java
+++ src/Parser.java
@@ -5,14 +5,14 @@
 
 public class Parser {
 	
-
-	private final static String MESSAGE_ERROR_INVALID_INPUT = "An error has occured. Please try again.";
-	private final static String MESSAGE_ERROR_INVALID_KEYWORD = "Invalid Keyword. Please try again";
+	private final static String MESSAGE_WRONG_USAGE = "Invalid command. Type <help> for assistance.";
+	private final static String[] MESSAGE_HELP = {"Task Panda v0.1 help manual", "blah blah", "more blah blah"};
 	private final static String COMMAND_ADD = "add";
 	private final static String COMMAND_DELETE = "delete";
 	private final static String COMMAND_EDIT = "edit";
 	private final static String COMMAND_LIST = "list";
 	private final static String COMMAND_UNDO = "undo";
+	private final static String COMMAND_HELP = "help";
 	
 	private static Integer NUMBER_COMMAND_INDEX = 0;
 	private static Integer NUMBER_USER_INPUT_WITHOUT_COMMAND = 1;
@@ -24,34 +24,37 @@ public class Parser {
 	 * Postcondition: passes command type and user input into executeCommand method    
 	 */
 	public void parseUserInput(String userInput) {
+		if(isInvalidString(userInput)) {
+			showToUser(MESSAGE_WRONG_USAGE);
+			return;
+		}
 		String userCommand = getCommand(userInput);
-		String userTask = removeFirstWord(userInput);
-		executeCommand(userCommand, userTask);
+		executeCommand(userCommand, userInput);
 	}
 	
-	/*
-	 * Method will determine which execution method to call depending on the userCommand passed in
-	 * Post condition: executes method respectively based on user command
-	 */
+	// Method will execute different methods according to the user's command
 	private void executeCommand(String userCommand, String userInput) {
 		switch(userCommand) {
 			case(COMMAND_ADD):
-				executeAdd(userInput);
+				executeAdd(removeFirstWord(userInput));
 				break;
 			case(COMMAND_DELETE):
-				executeDelete(userInput);
+				executeDelete(removeFirstWord(userInput));
 				break;
 			case(COMMAND_EDIT):
-				executeEdit(userInput);
+				executeEdit(removeFirstWord(userInput));
 				break;
 			case(COMMAND_LIST):
-				executeList(userInput);
+				executeList(removeFirstWord(userInput));
 				break;
 			case(COMMAND_UNDO):
-				executeUndo(userInput);
+				executeUndo();
+				break;
+			case(COMMAND_HELP):
+				displayHelp();
 				break;
 			default:
-				showToUser(MESSAGE_ERROR_INVALID_KEYWORD);
+				showToUser(MESSAGE_WRONG_USAGE);
 				break;
 		}
 	}
@@ -61,50 +64,61 @@ public class Parser {
 
 	}
 
-	// Method will print out given argument
+	// Method will print out the given argument
 	private void showToUser(String outputString) {
 		System.out.println(outputString);
 	}
 
+	// This methods edits a task given by the user
 	private void executeEdit(String userInput) {
 	    TaskEdit taskEdit = new TaskEdit();
 	    taskEdit.execute(userInput);
 	}
 
+	// This methods deletes a task given by the user
 	private void executeDelete(String userInput) {
 	     TaskDelete taskDel = new TaskDelete();
 	     taskDel.execute(userInput);
 	}
 
-	/*  
-	 * Process of coding.
-	 */
+	// This methods adds a task given by the user
 	private void executeAdd(String userInput) {
 		TaskAdder taskAdder = new TaskAdder();
 		taskAdder.run(userInput);
 	}
 
-	/*
-	 *  Process of coding.
-	 */
-	private void executeUndo(String userInput) {
+	// This methods undo the last operation performed by the user
+	private void executeUndo() {
 		TaskUndo taskUndo = new TaskUndo();
-		taskUndo.run();
+		taskUndo.undoAction();
 	}
 	
-	/*
-	 *  This method removes the command word  
-	 */
+	// This method removes the command word of user input and returns the remaining string
 	private String removeFirstWord(String userInput) {
 		String[] stringArray = userInput.split(" ", 2);
 		return stringArray[NUMBER_USER_INPUT_WITHOUT_COMMAND];
 	}
 	
-	/*
-	 *  This method returns the command word from the user input given
-	 */
+	
+	// This method returns the command word from the user input given
 	private String getCommand(String userInput) {
 		String[] inputArray = userInput.split(" ");
 		return inputArray[NUMBER_COMMAND_INDEX];
+	}
+	
+	// This method displays the help manual on "help" user command
+	private void displayHelp() {
+		for(int i=0; i<MESSAGE_HELP.length; i++) {
+			showToUser(MESSAGE_HELP[i]);
+		}
+	}
+	
+	// This method checks for empty strings
+	private boolean isInvalidString(String userInput) {
+		userInput = userInput.trim();
+		if(userInput.length() == 0 || userInput == null) {
+			return true;
+		}
+		return false;
 	}
 }
