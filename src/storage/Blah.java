687d6b1eda927239b0c7d4b609fd9aace0832a84
Kaung Htet Zaw <emoosx@gmail.com>
2014-03-02 12:58:31 +0800

    Serializer and deserializer for joda timestamp

--- src/storage/Blah.java
+++ src/storage/Blah.java
@@ -6,5 +6,6 @@ public class Blah {
 	public static void main(String[] args) {
 		StorageHelper storage = new StorageHelper();
 		Task newTask = new Task("test Task", "woowhooo");
+		storage.addNewTask(newTask);
 	}
 }
--- src/storage/StorageHelper.java
+++ src/storage/StorageHelper.java
@@ -6,31 +6,35 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import org.joda.time.DateTime;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import common.DateTimeTypeConverter;
 
 import core.Task;
 
 public class StorageHelper {
 
-	protected static Gson gson;
-	protected static Writer writer;
-	
 	protected static final String FILENAME = "data.json";
-	
 	public static final String ERROR_FILE_CREATION = "Error in file creation";
 	public static final String ERROR_TASK_ADDITION = "Error in task addition";
-	
 
+	private Gson gson;
 	private File file;
 	
 	public StorageHelper() {
 		this.file = createOrGetFile(FILENAME);
+		this.gson = new GsonBuilder()
+			.registerTypeAdapter(DateTime.class, new DateTimeTypeConverter())
+			.enableComplexMapKeySerialization()
+			.create();
 	}
 
 	public void addNewTask(Task t) {
 		try(Writer writer = new OutputStreamWriter(new FileOutputStream(this.file), "UTF-8")) {
-			gson = new Gson();
-			gson.toJson(t, writer);
+//			gson.toJson(t, writer);
+			System.out.println(this.gson.toJson(t));
 		} catch(IOException e) {
 			throw new Error(ERROR_TASK_ADDITION);
 		}
