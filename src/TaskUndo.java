5776a3cf9e3c1270a67fab49dc7cc3f350b25c42
Matthew <Matthew@localhost>
2014-03-03 20:37:06 +0800

    Rename comments

--- src/TaskUndo.java
+++ src/TaskUndo.java
@@ -5,7 +5,7 @@ import core.Task;
  * This class performs undo operations
  * It will store deleted task objects into a stack 
  * Stack will be popped when undo is executed
- * V0.1 - Supports undo for 1 operation
+ * V0.1 - Supports undo for at least 1 operation
  */
 
 public class TaskUndo {
