b55fd46b5cce93d7c78a289d03fd55ea1e7104c7
Kaung Htet Zaw <emoosx@gmail.com>
2014-03-11 19:27:02 +0800

    UI modifications and a placeholder textfield.

--- src/view/TaskPandaUI.java
+++ src/view/TaskPandaUI.java
@@ -1,14 +1,19 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
@@ -18,7 +23,7 @@ import logic.CommandFactory;
 
 public class TaskPandaUI extends JFrame {
 	
-	protected JTextField inputField;
+	protected PlaceholderTextField inputField;
 	protected CommandFactory commandFactory; 
 
 	public TaskPandaUI() {
@@ -33,12 +38,17 @@ public class TaskPandaUI extends JFrame {
 	}
 	
 	private void initUI() {
-		JPanel panel = new JPanel(new BorderLayout());
 		
-		JPanel inputPanel = new JPanel();
-		inputField = new JTextField(50);
-		inputField.setPreferredSize(new Dimension(5, 50));
-		inputField.setFont(new Font("SansSerif", Font.BOLD, 20));
+		JPanel basic = new JPanel();
+		basic.setLayout(new BoxLayout(basic, BoxLayout.Y_AXIS));
+		add(basic);
+
+		JPanel topPanel = new JPanel(new BorderLayout(0, 0));
+		inputField = new PlaceholderTextField(40);
+		inputField.setPlaceholder("Hello there!");
+		inputField.setPreferredSize(new Dimension(450, 45));
+		inputField.setFont(new Font("Sans-Serif", Font.BOLD, 18));
+		inputField.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10)));
 		inputField.addKeyListener(new KeyAdapter() {
 			public void keyReleased(KeyEvent e) {
 				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
@@ -51,13 +61,40 @@ public class TaskPandaUI extends JFrame {
 			public void keyPressed(KeyEvent e) {
 			}
 		});
-		inputPanel.add(inputField);
+		JSeparator separator = new JSeparator();
+		separator.setForeground(Color.GRAY);
+		topPanel.add(inputField, BorderLayout.CENTER);
+		topPanel.add(separator, BorderLayout.SOUTH);
 		
-		panel.add(inputPanel);
-		panel.setBorder(new EmptyBorder(new Insets(1, 1, 1, 1)));
+		
+		String[] columnNames = {
+				"First Name",
+				"Last Name",
+				"Sport",
+				"# of Years",
+				"Vegetarian"
+		};
+		
+		Object[][] data = {
+            { "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false)},
+            { "John", "Doe", "Rowing", new Integer(3), new Boolean(true)},
+            { "Sue", "Black", "Knitting", new Integer(2), new Boolean(false)},
+            { "Jane", "White", "Speed reading", new Integer(20), new Boolean(true)},
+            { "Joe", "Brown", "Pool", new Integer(10), new Boolean(false)},
+		};
+		
+		JPanel feedbackPanel = new JPanel();
+		JTable table = new JTable(data, columnNames);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+
+
+		
+		
 		
 
-		add(panel);
+		add(topPanel);
+//		add(scrollPane);
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
