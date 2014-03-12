bbb4165cda00e39c2be45aeaa68eb42304e86836
Clement <Clement.Chong73@gmail.com>
2014-03-11 18:51:35 -0700

    Add doDelete and doEdit to CommandFactory. Refine test cases

--- src/test/TestEdit.java
+++ src/test/TestEdit.java
@@ -1,28 +1,28 @@
 package test;
 
 import static org.junit.Assert.*;
-import logic.TaskEdit;
+import logic.CommandFactory;
 
 import org.junit.Test;
 
 public class TestEdit {
 
-	TaskEdit task = new TaskEdit();
+	CommandFactory cFact = CommandFactory.INSTANCE;
 	@Test
 	
 		public void testEdit() {
 			
-	        //testEditCommand("test empty", "Usage: delete <number>", "");
-	       // testEditCommand("test number out of arraylist range", "Please choose a lower value", "4");
-	       // testEditCommand("test for positive number", "Please key in a positive number", "-2");
-	      //  testEditCommand("test for invalid number format", "Please key in an integer", "two");
+	        testEditCommand("test empty", "Usage: edit <index> <description> on <date> from <start time> to <end time>", "");
+	        testEditCommand("test number out of arraylist range", "Please choose another value", "4 meetingchanged on 28-2-2014 from 3pm to 4pm");
+	        testEditCommand("test for positive number", "Please key in a positive number", "-2 meetingchanged on 28-2-2014 from 3pm to 4pm");
+	        testEditCommand("test for invalid number format", "Please key in an integer", "one meetingchanged on 28-2-2014 from 3pm to 4pm");
 			testEditCommand("test edit", "meetingchanged1516", "1 meetingchanged on 28-2-2014 from 3pm to 4pm");
 	
 		}
 
 		private void testEditCommand(String description, String expected,
 				String input) {
-			assertEquals(description, expected, task.testEdit(input));
+			assertEquals(description, expected, cFact.testEdit(input));
 		}
 	}
 
