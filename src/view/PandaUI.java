b53c2de610003dea9386f141cfa8f8c6ea75fb8c
Kaung Htet Zaw <emoosx@gmail.com>
2014-04-02 04:37:12 +0800

    Hook with Key Listener for Enter key

--- src/view/PandaUI.java
+++ src/view/PandaUI.java
@@ -1,18 +1,25 @@
 package view;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+import logic.Command;
+import logic.Command.COMMAND_TYPE;
 import logic.CommandFactory;
 import core.Task;
 
@@ -46,12 +53,35 @@ public class PandaUI extends Application {
 
 		inputField = new TextField();
 		inputField.setPromptText(TXT_PLACEHOLDER);
+		inputField.textProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				
+			}
+		});
+		
+		inputField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent e) {
+				if(e.getCode() == KeyCode.ENTER) {
+					Command command = new Command(inputField.getText());
+					if(command.command == COMMAND_TYPE.INVALID) {
+						System.out.println("Invalid command");
+					} else{
+						commandFactory.process(command);
+						updateTasksList();
+						inputField.clear();
+					}
+				}
+			}
+		});
 		
 		hbox.getChildren().addAll(inputField);
 		return hbox;
 	}
 	
 	private VBox addList() {
+
 		VBox vbox = new VBox();
 		vbox.setPadding(new Insets(PADDING));
 		vbox.setSpacing(8);
@@ -65,5 +95,10 @@ public class PandaUI extends Application {
         });
         vbox.getChildren().addAll(list);
         return vbox;
+	}
+	
+	private void updateTasksList() {
+		tasks = FXCollections.observableArrayList(commandFactory.getTasks());
+		list.setItems(tasks);
 	}
 }
