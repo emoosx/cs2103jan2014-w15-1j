162adaa78a2058dcdec32b9a24b0f2eeb08d943a
Matthew <Matthew@localhost>
2014-04-12 14:15:12 +0800

    add case insensitive for hybrid patterns

---
+++ src/logic/Criteria.java
@@ -0,0 +1,196 @@
+package logic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+
+import core.Task;
+
+/* 
+ * Class to help with the filtering of tasks
+ */
+public class Criteria {
+	
+	/* get all tasks that are not marked as deleted */
+	
+//	public static ArrayList<Task> getAllUndeletedTasks(List<Task> tasks) {
+//		Predicate<Task> undeletedTaskPredicate = new Predicate<Task>() {
+//			public boolean apply(Task t) {
+//				return t.getMarkAsDelete() == false;
+//			}
+//		};
+//		Collection<Task> collection = Collections2.filter(tasks, undeletedTaskPredicate);
+//		ArrayList<Task> result = new ArrayList<Task>(collection);
+//		return result;
+//	}
+	
+	public static ArrayList<Task> getAllOverdueTasks(List<Task> tasks) {
+		ArrayList<Task> result = new ArrayList<Task>();
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false && t.getTaskEndTime() != null && 
+			   t.getTaskEndTime().isBeforeNow())
+				result.add(t);
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllOverdueTaskIDs(List<Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false && t.getTaskEndTime() != null && 
+			   t.getTaskEndTime().isBeforeNow())
+				result.add(tasks.indexOf(t));
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllDoneTasks(List<Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == true) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	/* default criteria */
+	public static ArrayList<Integer> getAllUndeletedTasks(List<Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+
+	public static ArrayList<Integer> getAllUndeletedFloatingTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false) {
+				if(t.getTaskStartTime() == null && t.getTaskEndTime()== null)
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllFloatingTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+				if(t.getTaskStartTime() == null && t.getTaskEndTime()== null)
+				result.add(tasks.indexOf(t));
+			}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllUndeletedTimedTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false) {
+				if(t.getTaskStartTime() != null && t.getTaskEndTime() != null)
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllTimedTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+				if(t.getTaskStartTime() != null && t.getTaskEndTime() != null)
+				result.add(tasks.indexOf(t));		
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllUndeletedDeadlineTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+			if(t.getMarkAsDelete() == false && t.getTaskDone() == false) {
+				if(t.getTaskStartTime() == null && t.getTaskEndTime() != null)
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllDeadlineTasks(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks){
+				if(t.getTaskStartTime() == null && t.getTaskEndTime() != null)
+				result.add(tasks.indexOf(t));
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllTasksforToday(List<Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		DateTime today = new DateTime();
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete()==false && t.getTaskEndTime() != null && !t.getTaskDone() &&
+			   t.getTaskEndTime().withTimeAtStartOfDay().isEqual(today.withTimeAtStartOfDay())) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		System.out.println(result.size());
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllTasksforTomorrow(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		DateTime today = new DateTime();
+		DateTime tomorrow = today.plusDays(1);
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskEndTime() != null && !t.getTaskDone() && 
+               t.getTaskEndTime().withTimeAtStartOfDay().isEqual(tomorrow.withTimeAtStartOfDay())) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllTasksforThisWeek(List <Task> tasks) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		DateTime today = new DateTime();
+		DateTime nextWeek = today.plusDays(1);
+		for(Task t: tasks) {
+			if(t.getMarkAsDelete() == false && t.getTaskEndTime() != null && t.getTaskDone() == false &&
+			   t.getTaskEndTime().withTimeAtStartOfDay().isEqual(nextWeek.withTimeAtStartOfDay())) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllUndeletedTasksWithHashTag(List<Task> tasks, String rawText) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks) {
+			if(t.getTaskTags().contains(rawText) && !t.getTaskDone()) {
+				result.add(tasks.indexOf(t));
+			}
+		}
+		return result;
+	}
+	
+	public static ArrayList<Integer> getAllUndeletedTasksWithTimestamp(List<Task> tasks, DateTime inputDate) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(Task t: tasks) {
+			if(t.getTaskStartTime() == null && t.getTaskEndTime() != null && t.getMarkAsDelete() == false) {
+				if(t.getTaskEndTime().withTimeAtStartOfDay().isEqual(inputDate.withTimeAtStartOfDay())) {
+					result.add(tasks.indexOf(t));
+				}
+
+			} else if(t.getTaskStartTime() != null && t.getTaskEndTime() != null && t.getMarkAsDelete() == false) {
+                Interval interval = new Interval(t.getTaskStartTime(), t.getTaskEndTime());
+                if(interval.contains(inputDate)) {
+                	result.add(tasks.indexOf(t));
+                }
+			}
+		}
+		return result;
+	}
+}
---
+++ src/view/TaskCell.java
@@ -0,0 +1,176 @@
+package view;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import logic.CommandFactory;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import core.Task;
+
+public class TaskCell extends ListCell<Task> {
+
+	private static final int OFFSET = 1;
+
+	// grid
+	private static final int VGAP = 5;
+	private static final int INDEX_WIDTH = 40;
+	private static final int INDEX_HEIGHT = 60;
+
+	// css id for UI elements
+	private static final String HBOX_ID = "hbox";
+	private static final String GRID_ID = "grid";
+	private static final String DESC_ID = "description";
+	private static final String HASHTAG_ID = "hashtag";
+	private static final String INDEX_ID = "index";
+	private static final String START_ID = "start";
+	private static final String END_ID = "end";
+	private static final String TIME_CLASS = "timestamp";
+	private static final String OVERDUE_ID = "overdue-task";
+	private static final String FLOATING_ID = "floating";
+	private static final String DEADLINE_ID = "deadline";
+	private static final String TIMED_ID = "timed";
+	private static final String DONE_ID = "done";
+	private static final String SEPARATOR_ID = "separator";
+
+	private GridPane grid = new GridPane();
+	private HBox hbox = new HBox();
+	private Label index = new Label();
+	private Label desc = new Label();
+	private Label start = new Label();
+	private Label end = new Label();
+	private Label hashtag = new Label();
+	private Separator separator = new Separator(Orientation.VERTICAL);
+	private ObservableList<Task> tasks = CommandFactory.INSTANCE.getDisplayTasks();
+
+	public TaskCell() {
+		configureGrid();
+		configureHBox();
+		configureIndex(); // id label
+		configureDesc(); // description label
+		configureTimestamp();
+		configureSeparator();
+		configureHashtag();
+		addControls();
+	}
+	
+
+	private void configureGrid() {
+		grid.setHgap(VGAP);
+		grid.setVgap(VGAP);
+		grid.setId(GRID_ID);
+		grid.getColumnConstraints().addAll(new ColumnConstraints(),
+				new ColumnConstraints(120));
+	}
+
+	private void configureHBox() {
+		hbox.setId(HBOX_ID);
+	}
+
+	private void configureIndex() {
+		index.setPrefSize(INDEX_WIDTH, INDEX_HEIGHT);
+		index.setId(INDEX_ID);
+		index.setAlignment(Pos.CENTER);
+	}
+
+	private void configureDesc() {
+		desc.setId(DESC_ID);
+	}
+
+	private void configureTimestamp() {
+		start.getStyleClass().add(TIME_CLASS);
+		start.setId(START_ID);
+		end.setId(END_ID);
+		end.getStyleClass().add(TIME_CLASS);
+	}
+
+	private void configureHashtag() {
+		hashtag.setId(HASHTAG_ID);
+	}
+	
+	private void configureSeparator() {
+		separator.setId(SEPARATOR_ID);
+	}
+
+	private void addControls() {
+		grid.add(index, 0, 0, 1, 3);
+		grid.add(desc, 1, 0, 3, 1);
+		grid.add(start, 1, 1, 1, 1);
+		grid.add(end, 3, 1, 1, 1);
+//		grid.gridLinesVisibleProperty().set(true);
+		hbox.getChildren().addAll(grid, separator, hashtag);
+		HBox.setHgrow(grid, Priority.ALWAYS);
+	}
+
+	private void addContent(Task task) {
+		setText(null);
+  
+		index.setText(String.valueOf(super.indexProperty().get() + OFFSET));
+		desc.setText(task.getTaskDescription());
+		desc.setMaxWidth(PandaUI.APP_WIDTH - 100);
+		if(task.getTaskTags().isEmpty()) {
+			separator.setVisible(false);
+		}
+		hashtag.setText(task.getTags());
+
+		if (task.getTaskDone() == false) {
+			if (task.getLabel().equals("D")) {
+				hbox.setId(DEADLINE_ID);
+			} else if (task.getLabel().equals("T")) {
+				hbox.setId(TIMED_ID);
+			} else {
+				hbox.setId(FLOATING_ID);
+			}
+		} else {
+			hbox.setId(DONE_ID);
+		}
+
+		// overdue status
+		if (task.isOverdue()) {
+			// hbox.getStyleClass().add(OVERDUE_CLASS);
+			hbox.setId(OVERDUE_ID);
+		}
+
+		DateTimeFormatter fmt = DateTimeFormat.forPattern("d MMM yy  hh:mm a");
+		DateTime startTimestamp = task.getTaskStartTime();
+		if(startTimestamp == null) {
+			start.setText("");
+		} else {
+			start.setText(fmt.print(startTimestamp));
+		}
+
+		DateTime endTimestamp = task.getTaskEndTime();
+		if (endTimestamp == null) {
+			end.setText("");
+		} else {
+			end.setText(fmt.print(endTimestamp));
+		}
+
+		setGraphic(hbox);
+	}
+
+	private void clearContent() {
+		setText(null);
+		setGraphic(null);
+	}
+
+	@Override
+	public void updateItem(Task task, boolean empty) {
+		super.updateItem(task, empty);
+		if (empty) {
+			clearContent();
+		} else {
+			addContent(task);
+		}
+	}
+}
