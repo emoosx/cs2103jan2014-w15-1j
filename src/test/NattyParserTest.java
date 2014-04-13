f3e5981de5e44579883ccf12b5a988caab7c66ab
Matthew <Matthew@localhost>
2014-04-13 20:16:36 +0800

    refactor comments and test cases

--- src/test/NattyParserTest.java
+++ src/test/NattyParserTest.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 
 //@author A0101810A
 /*
- * NattyParserTest is a unit test to ensure that the strings with keywords contained inside are parsed correctly
- * This test class tests mainly on the NattyParser methods used in TaskParser.java
+ * NattyParserTest is a unit test to ensure that the strings extracted from RegExp.java are parsed correctly in NattyTime
  * Written by A0101810A - Tan Zheng Jie (Matthew)
  */
 
@@ -35,27 +34,27 @@ public class NattyParserTest {
 		assertEquals(2014,date[2]);
 		
 		// Partitioned test cases: DD/MM/YY format
-		date = TaskParser.dateFromDateString("on 12-2-2014");
+		date = TaskParser.dateFromDateString("12-2-2014");
 		assertEquals(12,date[0]);
 		assertEquals(2,date[1]);
 		assertEquals(2014,date[2]);
 		
-		date = TaskParser.dateFromDateString("on 12/2/2014");
+		date = TaskParser.dateFromDateString("12/2/2014");
 		assertEquals(12,date[0]);
 		assertEquals(2,date[1]);
 		assertEquals(2014,date[2]);
 		
-		// Partitioned test cases: 
+		// Partitioned test cases: relative date format
 		DateTime datetime = new DateTime();
 		datetime = datetime.plusDays(1);
-		date = TaskParser.dateFromDateString("by tomorrow 5pm");
+		date = TaskParser.dateFromDateString("tomorrow 5pm");
 		assertEquals(datetime.getDayOfMonth(),date[0]);
 		assertEquals(datetime.getMonthOfYear(),date[1]);
 		assertEquals(datetime.getYear(),date[2]);
 		
 		datetime = new DateTime();
 		datetime = datetime.plusDays(2);
-		date = TaskParser.dateFromDateString("on the day after tomorrow 5pm");
+		date = TaskParser.dateFromDateString("the day after tomorrow 5pm");
 		assertEquals(datetime.getDayOfMonth(),date[0]);
 		assertEquals(datetime.getMonthOfYear(),date[1]);
 		assertEquals(datetime.getYear(),date[2]);
@@ -64,19 +63,38 @@ public class NattyParserTest {
 	@Test
 	public void testGetTimeFromTimeString() {
 		int[] time = new int[2];
-		time = TaskParser.timeFromTimeString("on 12:12pm");
+		
+		time = TaskParser.timeFromTimeString("12:12pm");
 		assertEquals(12,time[0]);
 		assertEquals(12,time[1]);
 		
-		time = TaskParser.timeFromTimeString("on 1:12pm");
+		time = TaskParser.timeFromTimeString("1:12pm");
 		assertEquals(13,time[0]);
 		assertEquals(12,time[1]);
 		
-		time = TaskParser.timeFromTimeString("on 1:12am");
+		time = TaskParser.timeFromTimeString("1:12am");
 		assertEquals(1,time[0]);
 		assertEquals(12,time[1]);
 		
-		time = TaskParser.timeFromTimeString("on 1pm");
+		time = TaskParser.timeFromTimeString("1pm");
 		assertEquals(13,time[0]);
+		assertEquals(0, time[1]);
+		
+		time = TaskParser.timeFromTimeString("1pm");
+		assertEquals(13,time[0]);
+		assertEquals(0, time[1]);
+		
+		time = TaskParser.timeFromTimeString("1pm");
+		assertEquals(13,time[0]);
+		assertEquals(0, time[1]);
+		
+		time = TaskParser.timeFromTimeString("2359");
+		assertEquals(23,time[0]);
+		assertEquals(59, time[1]);
+		
+		time = TaskParser.timeFromTimeString("23:59");
+		assertEquals(23,time[0]);
+		assertEquals(59, time[1]);
+		
 	}
 }
