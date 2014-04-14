f66be50ee9a6fcbff987f45b8b7d22435735e9e6
Kaung Htet Zaw <emoosx@gmail.com>
2014-04-14 13:02:30 +0800

    UI code tagged for collation.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 import view.TaskPandaUI;
 
+//@author A0105860L-unused
+
+// now longer used from V0.3 onwards since we switched to javafx instead of Swing
+// current mainly class is in view/PandaUI.java
 public class Main {
 
 	public static void main(String[] args) {
