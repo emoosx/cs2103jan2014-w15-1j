cc17dbd427f7ee14f5757ccb72df7df79fdc6aef
Kaung Htet Zaw <emoosx@gmail.com>
2014-04-14 19:03:13 +0800

    Test files except commandfactorytest are finalized

--- src/test/CommandTest.java
+++ src/test/CommandTest.java
@@ -10,18 +10,34 @@ import logic.Command;
 import org.junit.Before;
 import org.junit.Test;
 
+//@author A0105860L
+/* test whether a command is created correctly from the user's input
+ * desired result is strip out the command verb from the input and create a command object
+ * the result of the string goes into attribute of the command as command.rawText
+ */
 public class CommandTest {
 	
 	private String c0Txt = "";
-	private String c1Txt = "add";
-	private String c2Txt = "add ";
-	private String c3Txt = "list  ";
+	private String c1Txt = "add";			// test whether it works with no space
+	private String c2Txt = "add ";			// one space
+	private String c3Txt = "list #project, 14 apr";		// multiple space
+	private String c4Txt = "add hackathon from 12:00 to 13:00";
+	private String c5Txt = "done 1";
+	private String c6Txt = "delete 1";
+	private String c7Txt = "delete lajalsjfdsa";
+	private String c8Txt = "undone 5";
+	private String c9Txt = "edit xxxx yyy ggg ";
 	
 	private Command command0;
 	private Command command1;
 	private Command command2;
 	private Command command3;
 	private Command command4;
+	private Command command5;
+	private Command command6;
+	private Command command7;
+	private Command command8;
+	private Command command9;
 	
 	@Before
 	public void setUp() {
@@ -29,7 +45,12 @@ public class CommandTest {
 		command1 = new Command(c1Txt);
 		command2 = new Command(c2Txt);
 		command3 = new Command(c3Txt);
-		command4 = new Command("add hackathon from 12:00 to 13:00");
+		command4 = new Command(c4Txt);
+		command5 = new Command(c5Txt);
+		command6 = new Command(c6Txt);
+		command7 = new Command(c7Txt);
+		command8 = new Command(c8Txt);
+		command9 = new Command(c9Txt);
 	}
 	
 	public void testAttemptAutoComplete() {
@@ -54,17 +75,27 @@ public class CommandTest {
 		String[] expected2 = {"add", null};
 		
 		String[] result3 = (String[])m.invoke(command3, c3Txt);
-		String[] expected3 = {"list", null};
+		String[] expected3 = {"list", "#project, 14 apr"};
+
+		String[] result4 = (String[])m.invoke(command4, c4Txt);
+		String[] expected4 = {"add", "hackathon from 12:00 to 13:00"};
+			
+		String[] result5 = (String[])m.invoke(command5, c5Txt);
+		String[] expected5 = {"done", "1"};
+
+		String[] result6 = (String[])m.invoke(command6, c6Txt);
+		String[] expected6 = {"delete", "1"};
+
+		String[] result7 = (String[])m.invoke(command7, c7Txt);
+		String[] expected7 = {"delete", "lajalsjfdsa"};				// still get the invalid string, but won't be executed
 
 		assertArrayEquals("command 0", expected, result);
 		assertArrayEquals("command 1", expected1, result1);
 		assertArrayEquals("command 2", expected2, result2);
 		assertArrayEquals("command 3", expected3, result3);
+		assertArrayEquals("command 4", expected4, result4);
+		assertArrayEquals("command 5", expected5, result5);
+		assertArrayEquals("command 5", expected6, result6);
+		assertArrayEquals("command 6", expected7, result7);
 	}
-	
-
-	public void testCreateCommand() {
-		
-	}
-
 }
--- src/test/CriteriaTest.java
+++ src/test/CriteriaTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import logic.Criteria;
+import logic.TaskParser;
 
 import org.joda.time.DateTime;
 import org.junit.AfterClass;
@@ -15,6 +16,11 @@ import org.junit.Test;
 
 import core.Task;
 
+//@author A0105860L
+/* test class for testing of filtering by different which is our main feature in listing */
+/* also serves as an integration test to make sure parser is parsing the details correctly
+ * upon create a new Task object
+ */
 public class CriteriaTest {
 	
 	private static List<Task> tasks;
@@ -30,6 +36,8 @@ public class CriteriaTest {
 		"next week stuffs by 22 apr 2014",
 		"go to school tomorrow from 2pm to 6pm"						// already overdue
 	};
+	
+	private static String FILLER = "by ";
 
 	@Before
 	public void setUp() throws Exception {
@@ -115,59 +123,66 @@ public class CriteriaTest {
 
 	@Test
 	public void testGetAllUndeletedTimedTasks() {
-		ArrayList<Integer> result = Criteria.getAllUndeletedFloatingTasks(tasks);
-		assertEquals("[5]", result.toString());
-		
-		tasks.get(5).setTaskEndTime(new DateTime());
-		result = Criteria.getAllUndeletedFloatingTasks(tasks);
-		assertEquals("[]", result.toString());
-		
-		// deleted tasks don't count
-		tasks.get(5).setTaskEndTime(null);
-		tasks.get(5).setMarkAsDelete();
-		result = Criteria.getAllUndeletedFloatingTasks(tasks);
-		assertEquals("[]", result.toString());
+		ArrayList<Integer> result = Criteria.getAllUndeletedTimedTasks(tasks);
+		assertEquals("[2, 4, 7]", result.toString());
 	}
 
 
 	@Test
 	public void testGetAllUndeletedDeadlineTasks() {
-		fail("Not yet implemented");
+		tasks.get(0).setMarkAsDelete();
+		ArrayList<Integer> result = Criteria.getAllUndeletedDeadlineTasks(tasks);
+		assertEquals("[1, 3, 6]", result.toString());
 	}
 
 	@Test
 	public void testGetAllDeadlineTasks() {
-		fail("Not yet implemented");
+		ArrayList<Integer> result = Criteria.getAllDeadlineTasks(tasks);
+		assertEquals("[0, 1, 3, 6]", result.toString());
 	}
 
+	// mark as done/delete should be working fine.
+	// start testing timestamp comparison from here onwards
 	@Test
 	public void testGetAllTasksforToday() {
-		fail("Not yet implemented");
+		ArrayList<Integer> result = Criteria.getAllTasksforToday(tasks);
+		assertEquals("[7]", result.toString());
 	}
 
 	@Test
 	public void testGetAllTasksforTomorrow() {
-		fail("Not yet implemented");
+		ArrayList<Integer> result = Criteria.getAllTasksforTomorrow(tasks);
+		assertEquals("[1]", result.toString());
 	}
 
 	@Test
 	public void testGetAllTasksforThisWeek() {
-		fail("Not yet implemented");
+		// this week = Interval between (today, today.plusWeeks(1));
+		ArrayList<Integer> result = Criteria.getAllTasksforThisWeek(tasks);
+		assertEquals("[1, 3]", result.toString());
 	}
 
 	@Test
 	public void testGetAllTasksforNextWeek() {
-		fail("Not yet implemented");
+		// next week = Interval between (today.plusWeeks(1), today.plusWeeks(2));
+		ArrayList<Integer> result = Criteria.getAllTasksforNextWeek(tasks);
+		assertEquals("[2, 6]", result.toString());
 	}
 
 	@Test
 	public void testGetAllUndeletedTasksWithHashTag() {
-		fail("Not yet implemented");
+		ArrayList<Integer> result = Criteria.getAllUndeletedTasksWithHashTag(tasks, "#project");
+		assertEquals("[1, 5]", result.toString());
 	}
 
+	// integration test, also tests whether our parser is parsing the input datetime correctly
 	@Test
 	public void testGetAllUndeletedTasksWithTimestamp() {
-		fail("Not yet implemented");
+		TaskParser parser = new TaskParser();
+		parser.parseTask(FILLER + "11 apr");
+		DateTime timestamp = parser.getEndDateTime();
+		ArrayList<Integer> result = Criteria.getAllUndeletedTasksWithTimestamp(tasks, timestamp);
+		assertEquals("[0]", result.toString());
 	}
 
 }
